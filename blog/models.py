7a980eb0674c176b8f22492ac9e5a3edd1183493
Aarón <aaronarrieropelaez99@gmail.com>
2019-03-05 16:31:29 +0100

    Separated production and dev requirements and installed django-pylint and autopep8. Created blog app. Defined models for the blog app.

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    """Tag or keyword that will be added to an entry to describe it."""
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}: {self.description[:10]}...'
+
+
+class Entry(models.Model):
+    """Any entry of the blog."""
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    summary = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    tag = models.ManyToManyField(Tag)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}: {self.summary[:10]}'
+
+
+class Profile(models.Model):
+    """Profile that will have extra info about the user."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_editor = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.user} is ' + ('not ' if not self.is_editor else '') + 'an editor'
