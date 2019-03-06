5f84eaf856483fcc81506d6939ed6ef7e246039c
Aarón <aaronarrieropelaez99@gmail.com>
2019-03-06 10:41:09 +0100

    Created basic index view and integrated Django authentication system

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,9 @@ class Entry(models.Model):
     def __str__(self):
         return f'{self.title} by {self.author}: {self.summary[:10]}'
 
+    class Meta:
+        verbose_name_plural = 'entries'
+
 
 class Profile(models.Model):
     """Profile that will have extra info about the user."""
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Entry
+
+class Index(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_entries_list'
+
+    def get_queryset(self):
+        return Entry.objects.order_by('-pub_date')
