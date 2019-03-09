7a8c2a94486f8ddd42c6326c1563e100f50c9471
Aarón <aaronarrieropelaez99@gmail.com>
2019-03-09 20:08:31 +0100

    Changed main serif font and finished styling index content view for desktops.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name = 'blog'
-urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
+AUTH_PATTERNS = [
     path('login/', auth_views.LoginView.as_view(
         template_name='blog/auth/login.html',
         redirect_authenticated_user=True
@@ -14,9 +12,19 @@ urlpatterns = [
         template_name='blog/auth/logout.html'
     ), name='logout'),
     path('signup/', views.Signup.as_view(), name='signup'),
-    path('editorcp/', views.editorcp_index, name='editorcp'),
-    path('editorcp/entries/new/', views.EditorcpCreateEntry.as_view(), name='create_entry'),
-    path('editorcp/entries/', views.EditorcpListEntries.as_view(), name='list_entries'),
-    path('editorcp/entries/<int:pk>/', views.EditorcpEditEntry.as_view(), name='edit_entry'),
-    path('editorcp/entries/<int:pk>/remove/', views.EditorcpDeleteEntry.as_view(), name='delete_entry'),
+]
+
+EDITORCP_PATTERNS = [
+    path('', views.editorcp_index, name='editorcp'),
+    path('entries/new/', views.EditorcpCreateEntry.as_view(), name='create_entry'),
+    path('entries/', views.EditorcpListEntries.as_view(), name='list_entries'),
+    path('entries/<int:pk>/', views.EditorcpEditEntry.as_view(), name='edit_entry'),
+    path('entries/<int:pk>/remove/', views.EditorcpDeleteEntry.as_view(), name='delete_entry'),
+]
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
+    path('', include(AUTH_PATTERNS)),
+    path('editorcp/', include(EDITORCP_PATTERNS)),
 ]
